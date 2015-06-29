4fecd7f540a81cfd1208601f4af1f250b7febbc3
Frank Burnham <fra.burnham@gmail.com>
2015-06-29 03:13:52 -0400

    Working out maptree

--- tree.lisp
+++ tree.lisp
@@ -1,83 +1,96 @@
 (defpackage :cl-tree
   (:use :common-lisp))
 
-;; some notes on trees
-;; a tree is a list of nodes
-;; a node is a list whose first is the data and rest are children (as a tree)
-;; tree:
-;; ((node (child (grandchild))) (node2 (child (grandchild))))
-;; node:
-;; (node (child (grandchild)))
+(defvar shallow-tree '((node-root
+                        (node-depth-1 (node-depth-2))
+                        (node-depth-1 (node-depth-2))
+                        (node-depth-1)
+                        (node-depth-1 (node-depth-2)))
+                       (node-root-2
+                        (child-node-1 (child-node-1-2))
+                        (child-node-2))
+                       leaf-root))
 
-(defvar tree '((node-root
-                (node-depth-1 (node-depth-2 (node-depth-3)))
-                (node-depth-1 (node-depth-2))
-                (node-depth-1)
-                (node-depth-1 (node-depth-2)))
-               (node-root-2
-                (child-node-1 (child-node-1-2))
-                (child-node-2))))
+(defun maptree (wind-fn unwind-fn tree)
+  (when tree
+    (let ((node (first tree)))
+      (let ((r (funcall wind-fn node)))
+        (funcall unwind-fn r)))))
+
+(defun println (data)
+  (format t "~a~%" data))
 
 (defun print-tabs (tabiness)
   "Print out tabs to make dfp prettier"
   (cond ((> tabiness 0)
          (format t "~T")
-         (print-tabs (- tabiness 1)))))
+         (print-tabs (1- tabiness)))))
 
-(defun dfp (tree &optional (tabiness 0))
-  "Depth first print of the tree"
-  (let ((node (first tree))
-        (node-data (first (first tree))))
-    (cond ((null tree) nil)
-          (t
-           (print-tabs tabiness)
-           (format t "~a~%" node-data)
-           (if (rest node)
-               (dfp (rest node) (+ 1 tabiness)))))
-    (if (rest tree)
-        (dfp (rest tree) tabiness))))
+(defun leafp (node)
+  "Return true if the node is a leaf"
+  (not (listp node)))
 
-(defun dfs (tree search-data)
-  "Depth first search of tree looking for search-data"
-  (let ((node (first tree))
-        (node-data (first (first tree))))
-    (let ((found
-           (cond ((null tree) nil)
-                 ((equal node-data search-data) node)
-                 (t
-                  (if (rest node) ;search down this node until you hit the bottom
-                      (dfs (rest node) search-data))))))
-      (if (and (null found) ;didn't find the search-data
-               (rest tree)) ;and the tree is not empty (just the path down the node was)
-          (dfs (rest tree) search-data) ;so search the rest of the tree
-          found)))) ;or return what you've found
+(defun branchp (node)
+  "Return true if the node is a branch"
+  (listp node))
 
-(defun ->tree (node)
-  "Convert a node into a tree"
-  (list node))
+(defun node-equalp (node data &key (equal-fn #'equal))
+  (cond ((branchp node)
+         (funcall equal-fn (first node) data))
+        ((leafp node)
+         (funcall equal-fn node data))))
 
-(defun ->node (tree)
-  "Convert a tree into a node"
-  (first tree))
+;; this is an awful name.
+(defun node-identity (node new-data)
+  "Return new-data.
+  Takes the same args that will be given to any fn passed as :update-fn to update-node."
+  new-data)
 
-(defun update-children (node child)
-  "Add a child to the node"
-  (let ((children (rest node))
-        (node-data (first node)))
-    (cons node-data (cons child children))))
+(defun dfprint (tree &optional (tabiness 0))
+  (maptree (lambda (node)
+             (print-tabs tabiness)
+             (cond ((branchp node)
+                    (println (first node))
+                    (cons (first node) (map-print (rest node) (1+ tabiness))))
+                   (t
+                    (println node)
+                    node)))
+           (lambda (node)
+             (cons node (map-print (rest tree) tabiness)))
+           tree))
 
-(defun add-child (tree parent-data child)
-  "Add a child to the given node (found by its data)
-   on the given tree"
-  ;; find the node, update the children, rebuild the tree
-  (let ((node (first tree))
-        (node-data (first (first tree))))
-    ;; the below finds the node and updates the child, now wrap it to
-    ;; rebuild the tree.
-    (cond ((equal node-data parent-data) (update-children node child))
-          ((null tree) nil)
-          (t
-           (if (rest node)
-               (add-child tree parent-data child))))
-    )
-  )
+(defun replace-all (tree search-data new-data)
+  (maptree (lambda (node)
+             (cond ((equal search-data node) new-data)
+                   ((branchp node)
+                    (if (equal search-data (first node))
+                        (cons new-data (rest node))
+                        (cons (first node) (replace-all (rest node) search-data new-data))))
+                   (t node)))
+           (lambda (node)
+             (cons node (replace-all (rest tree) search-data new-data)))
+           tree))
+
+(defun replace-first (tree search-data new-data)
+  (maptree ;; maybe replace first is a reduce
+   ;; build the new tree and stop replacing once one has been replaced
+   ;; try writing it this way to see if it clarifies anything in the process
+   ;; for me. I'll need to know trees very well to work on gp. Make the trees make some sense!
+   (lambda (node)
+     (cond ((equal search-data node) (list new-data t))
+           ((branchp node)
+            (if (equal search-data (first node))
+                (list (cons new-data (rest node)) t) ; the node has been found!
+                (let ((r (replace-first (rest node) search-data new-data)))
+                  (let ((node (first r))
+                        (found (second r)))
+                    (list (cons (first node) (replace-first (rest node) search-data new-data)) found)))))
+           (t (list () nil))))
+   (lambda (node)
+     (let ((node (first node))
+           (found (second node)))
+       (if found
+           (cons node (rest tree)) ;; found the node, stop searching
+           ;; the problem is in the code below? it's in the unwind somewhere I think
+           (cons node (replace-first (rest tree) search-data new-data)))))
+   tree))
